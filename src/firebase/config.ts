747e9fed777e7941cf228783fdaca60ad0a56118
Angel Tawe <angeltawe7@gmail.com>
2026-03-14 14:59:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9901977998-9ef79",
+  "appId": "1:529917883264:web:476ef13a879ccd870c4765",
+  "apiKey": "AIzaSyAVSRpakLUjT54dzPgOk9W8wWT4VMLZYpo",
+  "authDomain": "studio-9901977998-9ef79.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "529917883264"
+};
